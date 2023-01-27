c99b02abbaee27b018cb854432060baf5889b4d9
dylantheberry <dylantheberry02@gmail.com>
2023-01-26 19:01:58 -0600

    Phase 4

--- all-dragons.js
+++ all-dragons.js
@@ -1,10 +1,35 @@
-// Your code here
+const Dragon = require('../PFW-04-CDLP/classes/dragon')
 
+const FriendlyDragon = require('../PFW-04-CDLP/classes/friendly-dragon')
 
+const EvilDragon = require('../PFW-04-CDLP/classes/evil-dragon')
+
+const falkor = new FriendlyDragon(
+  "Falkor",
+  "white",
+  ["save Atreyu from the swamp",
+    "save Atreyu from the Nothing",
+    "scare the local bullies into a dumpster",
+  ],
+  "Bastian"
+)
+
+const smaug = new EvilDragon(
+  "Smaug",
+  "black",
+  ["take over your mountain kingdom",
+    "steal all your dwarven gold",
+    "burn down your floating village",
+  ],
+  "Dwarf King"
+)
+
+const allDragons = Dragon.getDragons(falkor, smaug)
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
 try {
+  module.exports.allDragons = allDragons
   module.exports.falkor = falkor
   module.exports.smaug = smaug
 } catch {
