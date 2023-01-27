3e584a219aa860ffd01e3f88e2ff895893f98ec3
dylantheberry <dylantheberry02@gmail.com>
2023-01-26 18:31:44 -0600

    Friendly Dragon

--- classes/friendly-dragon.js
+++ classes/friendly-dragon.js
@@ -1,1 +1,21 @@
-// Your code here
+const Dragon = require(`../classes/dragon`)
+
+class FriendlyDragon extends Dragon {
+    constructor(name, color, lifeGoals, friend) {
+        super(name, color);
+        this.lifeGoals = lifeGoals
+        this.friend = friend
+    }
+    hasLifeGoals() {
+        let goals;
+        for (let i = 0; i < this.lifeGoals.length; i++) {
+            goals = this.lifeGoals[i]
+            console.log(`${this.name} likes to ${goals}`)
+        }
+    }
+    helpsPeople() {
+        return `${this.name} helps their friend ${this.friend}`
+    }
+}
+
+module.exports = FriendlyDragon
